0e5c50b064176c7b2ccf44a0266c07e865b84f7a
IndigoClay <you@example.none>
2018-05-08 03:15:04 -0700

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hwapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = new String(new char[]{'a'});
+            
+            Console.WriteLine(System.Environment.MachineName);
+        }
+    }
+}
